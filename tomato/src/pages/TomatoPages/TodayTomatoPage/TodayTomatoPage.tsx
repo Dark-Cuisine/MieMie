d50e7a9ed3936d4f1df55f864e009bc4c42ee63e
Nekoiii <10nekosa@gmail.com>
2021-05-21 16:24:12 +0900

    handle status when fail to get img url at indexPage

--- tomato/src/pages/TomatoPages/TodayTomatoPage/TodayTomatoPage.tsx
+++ tomato/src/pages/TomatoPages/TodayTomatoPage/TodayTomatoPage.tsx
@@ -13,12 +13,14 @@ import './TodayTomatoPage.scss'
 
 const app = getApp()
 const tomatoTypes = app.$app.globalData.tomatoTypes
+const marco = app.$app.globalData.macro
 
 /***
  * 今天的番茄
  */
 const TodayTomatoPage = (props) => {
   const dispatch = useDispatch();
+  const layoutManager = useSelector(state => state.layoutManager);
   const userManager = useSelector(state => state.userManager);
   const initState = {
     tomatoDay: null,
@@ -29,6 +31,94 @@ const TodayTomatoPage = (props) => {
   }
   const [state, setState] = useState(initState);
   const [tomatoCalendar, setTomatoCalendar] = useState(null);
+  useEffect(() => {//应对index页拿取图片失败//*unfinished 要和index页的函数合起来别copy两次
+    if (!(tomatoTypes && tomatoTypes[0].iconUrl.length > 0)) {
+      initIcons()
+    }
+    if (!(tomatoTypes &&
+      tomatoTypes[0].animationImgUrls.work.length > 0)) {
+      initAnimations()
+    }
+  }, [layoutManager.currentTabId]);
+  const initIcons = async () => {//*注: 记得要用fileId换取真实路径！！！！
+    let r_1 = await wx.cloud.callFunction({
+      name: 'get_temp_file_url',
+      data: {
+        fileList: tomatoTypes.map((it, i) => {//番茄图标
+          return it.icon_fileId
+        }).concat([app.$app.globalData.imgs.beginTomatoButton.fileId],//开始按钮图标
+          [app.$app.globalData.imgs.alphaChannel.fileId])
+      }
+    });
+    let urls = r_1.result || []
+    // console.log('urls', urls);
+    tomatoTypes.map((it, i) => {
+      it.iconUrl = urls[i]
+    })
+    app.$app.globalData.imgs.beginTomatoButton.fileUrl = urls[urls.length - 2]
+    app.$app.globalData.imgs.alphaChannel.fileUrl = urls[urls.length - 1]
+  }
+
+  const initAnimations = async () => {
+    let animationFileIds: string[] = []
+    for (let it of tomatoTypes) {
+      for (let i = 0; i < marco.ANI_WORK_LENGTH; ++i) {
+        animationFileIds.push(
+          'cloud://miemie-buyer-7gemmgzh05a6c577.6d69-miemie-buyer-7gemmgzh05a6c577-1304799026/resources/animations/growingTomato/'
+          + it.color + '/work_' + (i + 1) + '.png'
+        )
+      }
+      for (let i = 0; i < marco.ANI_TRANS_LENGTH; ++i) {
+        animationFileIds.push(
+          'cloud://miemie-buyer-7gemmgzh05a6c577.6d69-miemie-buyer-7gemmgzh05a6c577-1304799026/resources/animations/growingTomato/'
+          + it.color + '/trans_' + (i + 1) + '.png'
+        )
+      }
+      for (let i = 0; i < marco.ANI_REST_LENGTH; ++i) {
+        animationFileIds.push(
+          'cloud://miemie-buyer-7gemmgzh05a6c577.6d69-miemie-buyer-7gemmgzh05a6c577-1304799026/resources/animations/growingTomato/'
+          + it.color + '/rest_' + (i + 1) + '.png'
+        )
+      }
+    }
+    // console.log('animationFileIds',animationFileIds);
+    if (animationFileIds && animationFileIds.length > 0) {
+      try {
+        let r_1 = await wx.cloud.callFunction({
+          name: 'get_temp_file_url',
+          data: {
+            fileList: animationFileIds.slice(0,//一次最多只能拿50个所以分开拿
+              2 * (marco.A_TOTAL_LENGTH))
+          }
+        });
+        let r_2 = await wx.cloud.callFunction({
+          name: 'get_temp_file_url',
+          data: {
+            fileList: animationFileIds.slice(
+              2 * (marco.A_TOTAL_LENGTH))
+          }
+        });
+        if (!(r_1 && r_1.result && r_1.result.length > 0 &&
+          r_2 && r_2.result && r_2.result.length > 0)) { console.log('读取出错'); return; }
+        let urls = r_1.result.concat(r_2.result)
+        // console.log('',urls);
+        tomatoTypes.map((it, i) => {
+          it.animationImgUrls.work = urls.slice(i * (marco.A_TOTAL_LENGTH), i * (marco.A_TOTAL_LENGTH) + marco.ANI_WORK_LENGTH)
+          it.animationImgUrls.trans = urls.slice(i * (marco.A_TOTAL_LENGTH) + marco.ANI_WORK_LENGTH, i * (marco.A_TOTAL_LENGTH) + marco.ANI_WORK_LENGTH + marco.ANI_TRANS_LENGTH)
+          it.animationImgUrls.rest = urls.slice(i * (marco.A_TOTAL_LENGTH) + marco.ANI_WORK_LENGTH + marco.ANI_TRANS_LENGTH, i * (marco.A_TOTAL_LENGTH) + marco.ANI_WORK_LENGTH + marco.ANI_TRANS_LENGTH + marco.ANI_REST_LENGTH)
+        })
+      }
+      catch (err) {
+        console.log('c-get_temp_file_url-err', err);
+        wx.showToast({
+          title: '获取动画图片失败',
+          icon: 'none',
+        })
+        return err;
+      }
+      // console.log('-tomatoTypes', tomatoTypes);
+    }
+  }
 
   useEffect(() => {
     userManager.unionid && userManager.unionid.length > 0 &&
@@ -85,7 +175,7 @@ const TodayTomatoPage = (props) => {
       blueList: blueList,
       whiteList: whiteList,
     });
-   }
+  }
 
 
 
